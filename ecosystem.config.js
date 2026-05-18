958944eaa78db7e2a1da3fa58ff39610106c8d20
jessi <jessi@jessideMacBook-Pro.local>
2026-05-18 10:39:43 +0800

    chore: update production pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: "aiprint-web",
-      script: ".next/standalone/server.js",
-      cwd: "/www/wwwroot/aiprint.hichara.com",
+      name: "szsketch",
+      script: "node_modules/.bin/next",
+      args: "start -p 3000",
+      cwd: "/var/www/szsketch",
       env: {
         NODE_ENV: "production",
         HOSTNAME: "127.0.0.1",
@@ -11,8 +12,8 @@ module.exports = {
       },
       instances: 1,
       max_memory_restart: "512M",
-      error_file: "/www/wwwroot/aiprint.hichara.com/logs/pm2-error.log",
-      out_file: "/www/wwwroot/aiprint.hichara.com/logs/pm2-out.log",
+      error_file: "/var/log/szsketch/error.log",
+      out_file: "/var/log/szsketch/out.log",
       merge_logs: true,
       log_date_format: "YYYY-MM-DD HH:mm:ss",
     },
